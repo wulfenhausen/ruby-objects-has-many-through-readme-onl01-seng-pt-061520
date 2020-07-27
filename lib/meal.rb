e043a4cba2c2e65fe183164ba50862878722667f
William Han Findiesen <william.findiesen@gmail.com>
2020-07-27 01:49:23 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -3,7 +3,7 @@ class Meal
   
   @@all = []
   
-  def initalize(waiter, customer, total, tip=0)
+  def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
     @total = total
