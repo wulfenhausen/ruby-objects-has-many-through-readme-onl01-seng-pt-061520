a36389030173535288fca2f9d1155b03bbed8862
William Han Findiesen <william.findiesen@gmail.com>
2020-07-27 01:51:39 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -10,6 +10,12 @@ class Customer
     @@all << self
   end
   
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end 
+    
+    
+  
   def self.all
     @@all
   end
