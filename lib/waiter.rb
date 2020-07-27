77d3298eef570454590974fdd483bc1832c2ec95
William Han Findiesen <william.findiesen@gmail.com>
2020-07-27 01:43:35 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -2,13 +2,16 @@ class Waiter
   
   attr_accessor :name, :experience
   
+  @@all = []
+  
   def initialize(name, experience)
     @name = name
     @experience = experience
+    @@all << self
   end
   
   def self.all
-    
+    @@all
   end 
   
 end
